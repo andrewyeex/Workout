304cdafd0c570706a021f5162de998980566baa0
Andrew Yee <andrew.yee@mail.com>
2019-04-23 13:58:41 -0700

    added react-native-progress

--- components/WorkoutCard.js
+++ components/WorkoutCard.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { ImageBackground, Text, View, Image} from 'react-native'
+import { ImageBackground, Text, View, TouchableOpacity} from 'react-native'
 
 export class WorkoutCard extends React.PureComponent {
+  handleSelectedWorkout = () => this.props.handleSelectedWorkout({...this.props})
   render() {
     const {
       name,
@@ -9,13 +10,13 @@ export class WorkoutCard extends React.PureComponent {
       image
     } = this.props
     return (
-      <View style={{flexBasis: '50%', padding: 10 }}>
+      <TouchableOpacity style={{marginBottom: 10, padding: 10 }} onPress={this.handleSelectedWorkout}>
         <ImageBackground source={image} style={{height: 150, marginBottom: 10}} imageStyle={{borderRadius: 10}} />
         <View style={{flexWrap:'wrap', paddingHorizontal: 5}}>
           <Text style={{marginBottom: 5, fontWeight: '600'}}>{name}</Text>
           <Text style={{fontWeight: '100'}}>{description}</Text>
         </View>
-    </View>
+      </TouchableOpacity>
     )
   }
 }
